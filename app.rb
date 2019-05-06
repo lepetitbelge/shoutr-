07bb9197471d73fa079c27a0b800d71b4b5d34ef
christophegillis <chris.tim.gillis@gmail.com>
2019-05-06 19:39:21 +0200

    added preliminary modal to add a post

--- app.rb
+++ app.rb
@@ -10,16 +10,37 @@ configure :development do
 end
 
 get '/' do
-  @posts = DB
-  @comments = COMMENTS
-  @posts_with_comments = combine_posts_and_comments
+  posts = DB
+  comments = COMMENTS
+  @posts = combine_posts_and_comments(posts, comments)
   erb :index
+end
+
+post '/post_new' do
+  result = check_params(params)
+  DB << result && COMMENTS << [] unless result.nil?
+  puts "weet even niet hoe ik verder moet"
 end
 
 private
 
-def combine_posts_and_comments
-  @posts.each_with_index do |post, index|
-    post[:comments] = @comments[index]
+def combine_posts_and_comments(posts, comments)
+  posts.each_with_index.map do |post, index|
+    post.merge comments: comments[index]
   end
+end
+
+def check_params(params)
+  unless params[:title].nil? || params[:content].nil?
+    date = Time.now.strftime('%b%e')
+    new_post = {
+      title: params[:title],
+      content: params[:content],
+      photo: '/favicon.jpg',
+      rating: '',
+      writer: 'Anonymous',
+      date: date
+    }
+  end
+  new_post
 end
